777f038342a5f019dafa47867b69705ff429b93f
kingmozzi <kingmozzi_@naver.com>
2025-07-29 20:26:52 +0900

    Initial Commit

---
+++ ToDoList/Model/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Model
+{
+    class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+        public string Category { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+    }
+
+    public enum PriorityLevel
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2
+    }
+}
